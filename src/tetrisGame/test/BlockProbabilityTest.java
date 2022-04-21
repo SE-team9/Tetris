1d8ee6d404064fdcbe879e4535e91d7d2cf39d6d
funnysunny08 <88873302+funnysunny08@users.noreply.github.com>
2022-04-21 21:40:20 +0900

    I형 블록 확률 test

---
+++ src/tetrisGame/test/BlockProbabilityTest.java
@@ -0,0 +1,30 @@
+package tetrisGame.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import tetris.GameArea;
+import tetris.Tetris;
+
+class BlockProbabilityTest {
+
+	@RepeatedTest(10000)
+	void testMakeRandom() {
+
+		final GameArea ga = new GameArea(600, 450, 10);
+		ga.level = 0; // Easy mode일 때, Failures 수가 10000번 실행했을 때 I형 block이 나오는 횟수
+		// ga.level = 1; // Normal mode일 때
+		// ga.level = 2; // Hard mode일 때
+		
+		// when
+		final int r = ga.makeRandom();
+		
+		// then
+		assertTrue(r != 0);
+	}
+
+}
